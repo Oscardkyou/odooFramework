2ba4f1816d6a158635aec9c39433550d332c7bf7
«Oscardk» <dauren2050@yandex.ru>
2023-12-23 20:20:06 +0600

    add

--- my_module/__init__.py
+++ my_module/__init__.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of ProductMarkingSystem module
+{
+    'name': 'ProductMarkingSystem',
+    'version': '1.0',
+    'author': 'dk',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'data': [
+        'security/my_module_security.xml',
+        'views/my_model_views.xml',
+        'views/other_model_views.xml',
+        'data/my_module_data.xml',
+    ],
+    'demo': [
+        'data/my_module_demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
